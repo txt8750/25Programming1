fe110e63bc435f9502234a783966a940ef43d6ef
txt8750 <txt8750@gmail.com>
2025-10-16 21:08:04 +0900

    2025-10-16_Homework

--- SBSProgramming1/2_자료형과 변수/main.c
+++ SBSProgramming1/2_자료형과 변수/main.c
@@ -20,15 +20,16 @@
 
 
 #include <stdio.h>
+#include <Windows.h>
 
 int main() {
-
+	/*
 	// 더블킬 게임화면에 출력하고 싶다
-	printf("더블킬\n");
+	//printf("더블킬\n");
 	// 레드팀과 블루팀의 스코어를 출력하고싶다
-	printf("10 vs 25\n");
+	//printf("10 vs 25\n");
 	// 챔피언이 가지고 있는 스탯을 출력하고 싶다
-	printf("AD : 10\n\n");
+	//printf("AD : 10\n\n");
 
 	
 	//////////////////////////////////////
@@ -44,13 +45,13 @@ int main() {
 
 	float GarenFirstLvAS = 0.625f;
 	float GarenGrowthASvs = 0.0365 * 100;
-	float GarenGrowthAS = 0.0365;
+	float GarenGrowthAS = 0.0365f;
 
 	printf("가렌의 1레벨 능력치\n");
 	printf("체력: %d (+%d)\n", GarenFirstLvHP, GarenGrowthHP);
 	printf("마나: %d (+%d)\n", GarenFirstLvMP, GarenGrowthMP);
 	printf("공격력: %d (+%d)\n", GarenFirstLvAD, GarenGrowthAD);
-	printf("공격 속도: %.3f (+%.2f%)\n\n", GarenFirstLvAS, GarenGrowthASvs);	// float 출력시 소숫점 뒷자리 제거 하는 방법
+	printf("공격 속도: %.3f (+%.2f%%)\n\n", GarenFirstLvAS, GarenGrowthASvs);	// float 출력시 소숫점 뒷자리 제거 하는 방법
 
 
 
@@ -66,6 +67,7 @@ int main() {
 	printf("가렌 현재 공격 속도 : %.3f\n\n", GarenFirstLvAS + (GarenLevel - 1) * GarenGrowthAS);
 
 	// 다른 챔피언으로도 위의 방식으로 정보를 표현해보세요 (케이틀린)
+	
 	int CaitlynLevel = 5;
 	int CaitlynFirstLvHP = 580;
 	int CaitlynGrowthHP = 107;
@@ -85,22 +87,112 @@ int main() {
 	printf("케이틀린 현재 마나 : %d\n", CaitlynFirstLvMP + (CaitlynLevel - 1) * CaitlynGrowthMP);
 	printf("케이틀린 현재 공격력 : %d\n", CaitlynFirstLvAD + (CaitlynLevel - 1) * CaitlynGrowthAD);
 	printf("케이틀린 현재 공격 속도 : %.3f\n\n", CaitlynFirstLvAS + (CaitlynLevel - 1) * CaitlynGrowthAS);
+	*/
+
+	// 분석해보고 싶은 게임의 정보를 하나 가져와서 수식으로 표현 (레인보우 식스 시즈)
+	
+	char PlayerOperator[20] = "Grim";	// 플레이어의 오퍼레이터 명
+	char TeamOperator1[20] = "Kali";	// 팀원1의 오퍼레이터 명
+	char TeamOperator2[20] = "Hibana";  // 팀원2의 오퍼레이터 명
+	char TeamOperator3[20] = "Ying";    // 팀원3의 오퍼레이터 명
+	char TeamOperator4[20] = "Ash";     // 팀원4의 오퍼레이터 명
+
+	int AtkScore = 0;				// 공격팀 승리 횟수
+	int DefScore = 0;				// 방어팀 승리 횟수
+	int Round = 1;					// 현재 라운드
+
+	int PlayerMaxHP = 100;			// 플레이어의 최대 체력
+	int PlayerHP = 100;				// 플레이어의 현재 체력
+	int PlayerSpecAB = 5;			// 플레이어의 특수 능력 개수
+	int PlayerDrone = 2;			// 플레이어가 지니고 있는 드론 개수
+	int PlayerGadget = 2;			// 플레이어가 지니고있는 가젯 개수
+
+	int PlayerMainAmmo = 31;		// 플레이어의 메인 총에 장전 된 총알 개수
+	int PlayerSpareAmmo_M = 150;	// 플레이어의 메인 총에 장전 되어있지 않은 총알 개수
+	int PlayerSubAmmo = 16;			// 플레이어의 서브 총에 장전 된 총알 개수
+	int PlayerSpareAmmo_S = 60;		// 플레이어가 서브 총에 장전 되어있지 않은 총알 개수 
+
+	
+	// 2라운드 게임시작 스코어Atk 1 : Def 0
+
+	printf("플레이어 : %s\n", PlayerOperator);								//플레이어가 선택한 오퍼레이터 명 출력
+	printf("팀원1 : %s, 팀원2 : %s, 팀원3 : %s, 팀원4 : %s\n", TeamOperator1, TeamOperator2, TeamOperator3, TeamOperator4); //플레이어의 팀원들이 선택한 오퍼레이터 명 출력
+	printf("라운드%d\n\n",Round);											// 현재 라운드 출력
+	printf("현재 스코어\n Atk : %d\n Def : %d\n\n", AtkScore + 1, DefScore);	// 공격팀, 방어팀 현재 스코어 출력
 
 
-	// 분석해보고 싶은 게임의 정보를 하나 가져와서 수식으로 표현 (레이보우 식스 시즈)
-	int AtkScore = 0;
-	int DefScore = 0;
-	int Round = 1;
+	// 2라운드 진행 중 플레이어가 드론 모두 사용, 특수능력 3개 사용, 가젯 1개 사용
+	// 메인 무장 총알 20발 사용 후 재장전
+	// 서브 부장 총알 10발 사용 후 재장전
 
-	int PlayerFirstHP = 100;
-	int PlayerSpecAB = 5;
+	printf("게임 시작 직후 플레이어 상태\n");
+	printf("체력 : %d | %d\n", PlayerHP, PlayerMaxHP);
+	printf("드론 소지 개수 : %d\n", PlayerDrone);
+	printf("특수 능력 소지 개수 : %d\n", PlayerSpecAB);
+	printf("가젯 소지개수 : %d\n", PlayerGadget);
+	printf("메인 무장 총알 : %d\n", PlayerMainAmmo);
+	printf("메인 무장 예비 총알 : %d\n", PlayerSpareAmmo_M);
+	printf("서브 무장 총알 : %d\n", PlayerSubAmmo);
+	printf("서브 무장 예비 총알 : %d\n\n", PlayerSpareAmmo_S);
+	Sleep(1000);
+	printf("게임 진행\n\n");
+	Sleep(3000);
 
-	int MainGunAmmo = 5;
-	int SubGunAmmo = 13;
+	int PlayerGetDmg = 60;
+	PlayerHP -= PlayerGetDmg;
+	printf("체력 : %d | %d\n", PlayerHP, PlayerMaxHP);						// 60데미지를 받은 플레이어의 현재 체력
+
+	int PlayerUseDrone = 2;
+	PlayerDrone -= PlayerUseDrone;
+	printf("드론 소지 개수 : %d\n", PlayerDrone);							// 드론 모두 사용
+
+	int PlayerUseSpecAB = 3;
+	PlayerSpecAB = PlayerSpecAB - PlayerUseSpecAB;							// 특수능력 3번 사용
+	printf("특수 능력 소지 개수 : %d\n", PlayerSpecAB);
+
+	int PlayerUseGadget = 1;
+	PlayerGadget -= PlayerUseGadget;
+	printf("가젯 소지 개수 : %d\n", PlayerGadget);							// 가젯 1개 사용
+
+	int PlayerUseMainAmmo = 20;
+	PlayerMainAmmo = PlayerMainAmmo - PlayerUseMainAmmo;								
+	printf("메인 무장 총알 : %d\n", PlayerMainAmmo);							// 플레이어가 사용하고 남은 메인 무장 총알
+	printf("메인 무장 예비 총알 : %d\n", PlayerSpareAmmo_M);					// 플레이어의 메인 무장 예비 총알
+
+	int PlayerUseSubAmmo = 10;
+	PlayerSubAmmo = PlayerMainAmmo - PlayerUseSubAmmo;
+	printf("서브 무장 총알 : %d\n", PlayerSubAmmo);							// 플레이어가 사용하고 남은 서브 무장 총알
+	printf("서브 무장 예비 총알 : %d\n\n", PlayerSpareAmmo_S);				// 플레이어의 메인 무장 예비 총알
+
+	//메인 무장 재장전
+	Sleep(2000);
+	printf("메인 무장 재장전 진행\n");
+	Sleep(1000);
+	PlayerMainAmmo = PlayerMainAmmo - PlayerMainAmmo + 1;
+	printf("메인 무장 총알 : %d\n", PlayerMainAmmo);							// 재장전 시 약실에 남은 총알 제외한 나머지 총알 제거
+	Sleep(1000);
+	printf("재장전 완료\n\n");
+	Sleep(2000);
+	PlayerMainAmmo = PlayerMainAmmo + 30;
+	printf("메인 무장 총알 : %d\n", PlayerMainAmmo);							// 재장전 후 장전된 총알 개수
+	PlayerSpareAmmo_M = PlayerSpareAmmo_M - PlayerUseMainAmmo;
+	printf("메인 무장 예비 총알 : %d\n\n", PlayerSpareAmmo_M);				// 재장전 후 남은 예비 총알 개수
 
 
-
-
+	//서브 무장 재장전
+	Sleep(2000);
+	printf("서브 무장 재장전 진행\n");
+	Sleep(1000);
+	PlayerSubAmmo = PlayerSubAmmo - PlayerSubAmmo + 1;
+	printf("서브 무장 총알 : %d\n", PlayerSubAmmo);							// 재장전 시 약실에 남은 총알 제외한 나머지 총알 제거
+	Sleep(1000);
+	printf("재장전 완료\n\n");
+	Sleep(2000);
+	PlayerSubAmmo = PlayerSubAmmo + 15;
+	printf("서브 무장 총알 : %d\n", PlayerSubAmmo);							// 재장전 후 장전된 총알 개수
+	PlayerSpareAmmo_S = PlayerSpareAmmo_S - PlayerUseSubAmmo;
+	printf("서브 무장 예비 총알 : %d\n\n", PlayerSpareAmmo_S);				// 재장전 후 남은 예비 총알 개수
+	
 	return 0;
 
 }
